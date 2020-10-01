0cd049583516c3ccce20711887032427b4c2771e
Farzad M. Zarasvand <dev.farzadz@gmail.com>
2020-10-02 01:49:13 +0330

    Connecting to postgresql usting TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbConnectionService } from './db-connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DbConnectionService,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DbConnectionService],
 })
 export class AppModule {}
